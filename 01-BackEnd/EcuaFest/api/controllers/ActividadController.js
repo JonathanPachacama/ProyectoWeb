f99f0c26234a7d4c5d3a0746e6ff872d12d0399c
JesusNaula <daniel.naula@epn.edu.ec>
2017-08-18 22:56:48 -0500

    Borrar Actividad

--- 01-BackEnd/EcuaFest/api/controllers/ActividadController.js
+++ 01-BackEnd/EcuaFest/api/controllers/ActividadController.js
@@ -34,4 +34,20 @@ module.exports = {
             }
         });
     },
+    eliminarActividad: function (req, res) {
+        var params = req.allParams();
+        sails.log.info("Parametros", params);
+        if (req.method == "POST" && params.id) {
+            Actividad.destroy({
+                id: params.id
+            }).exec(function (err, ActividadBorrado) {
+                if (err)
+                    return res.serverError(err);
+                return res.redirect("/ecuafestDetalleActividades");
+            });
+        }
+        else {
+            return res.badRequest();
+        }
+    }
 };
